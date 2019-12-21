4095268ffeddf5f9ff3d75d8496bb3b79f91ec28
Mr-liu <you@example.com>
2019-12-21 11:50:10 +0800

    :lipstick: 优化样式 删除.idea文件夹

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -11,6 +11,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
--- src/components/app.tsx
+++ src/components/app.tsx
@@ -3,6 +3,8 @@ import Recorder from '../utils/recorder';
 import readAsDataURL from '../utils/readAsDataURL';
 import ASRConfig from '../api/asr-config';
 
+import './app.css';
+
 interface AppProps {
   /**
    * 控制点击开始录音和结束录音
@@ -31,7 +33,8 @@ export default class App extends React.Component<AppProps, AppProps> {
 
   // 开始
   startFunc = () => {
-    this.setState({
+    this.audioData = null;
+    return this.setState({
       recordFlag: false
     }, () => {
       // @ts-ignore
@@ -51,30 +54,42 @@ export default class App extends React.Component<AppProps, AppProps> {
       // @ts-ignore
       this.recorder.getSource().then(data => {
         readAsDataURL(data).then(data => {
-          this.audioData = data
           console.log(data, 'data');
+          this.audioData = data
+          // 播放
+          const { audio } = this;
+          audio.src = this.audioData;
         })
       })
     })
   };
 
-  //播放
-  playFunc = () => {
-    if(this.audioData) {
-      const { audio } = this;
-      audio.src = this.audioData;
-    }
-  }
-
   render(): React.ReactNode {
     const { recordFlag } = this.state;
     return (
-      <div>
+      <div className="box">
         <div>
           <audio className="audio" ref={ref => {this.audio = ref;}} controls autoPlay/>
         </div>
-        <button onClick={recordFlag ? () => {return this.startFunc()} : () => {return this.endFunc()}}>{recordFlag ? '开始' : '结束'}</button>
-        <button onClick={this.playFunc}>播放</button>
+        {recordFlag ? (
+          <div className="time-line-box"/>
+        ) : (
+          <div className="time-box">
+          <span className="start-taste-line">
+              <hr className="hr hr1" />
+              <hr className="hr hr2" />
+              <hr className="hr hr3" />
+              <hr className="hr hr4" />
+              <hr className="hr hr5" />
+              <hr className="hr hr6" />
+              <hr className="hr hr7" />
+              <hr className="hr hr8" />
+              <hr className="hr hr9" />
+              <hr className="hr hr10" />
+          </span>
+          </div>
+        )}
+        <button className="start-btn" onClick={recordFlag ? () => {return this.startFunc()} : () => {return this.endFunc()}}>{recordFlag ? '开始' : '结束'}</button>
       </div>
     )
   }
